a59180224f72fad2bad024832c46ba44a54c9bc2
ramiasg <rami.sweyri@gmail.com>
2022-09-10 11:04:37 +0300

    improve set method 2

--- index.js
+++ index.js
@@ -39,7 +39,9 @@ function State(state = {}) {
               ? value
               : typeof value === "string" || !isNaN(Number(p))
               ? value
-              : { ...this.resolvePath(path), ...value }
+              : typeof this.getPropertyPath(this.state, path) !== "undefined"
+              ? { ...this.resolvePath(path), ...value }
+              : { ...value }
             : o[p] || {});
       }, this.state);
     } else {
@@ -52,6 +54,25 @@ function State(state = {}) {
   };
   this.put = function (vlaue) {
     this.setPath(null, vlaue, true);
+  };
+  this.getPropertyPath = function (obj, path) {
+    if (!obj || !path) {
+      return;
+    }
+
+    if (typeof path === "string") {
+      path = path.split(".");
+    }
+
+    for (var i = 0; i < path.length - 1; i++) {
+      obj = obj[path[i]];
+
+      if (typeof obj === "undefined") {
+        return;
+      }
+    }
+
+    return obj[path.pop()];
   };
 
   this.deletePropertyPath = function (obj, path) {
@@ -140,4 +161,60 @@ function State(state = {}) {
   };
 }
 
+// const state = new State({});
+// const formData = new State({ firstName: "rami", lastName: "sweyri" });
+
+// formData.set("middleName", "asgm");
+// state.setState({
+//   email: "rami.sweyri@gmail.com",
+//   devices: [
+//     {
+//       id: 1,
+//       type: "laptop",
+//     },
+//   ],
+// });
+
+// state.set("devices.0.type", "PC");
+// state.set("user", {
+//   ...formData.get(), // or formData.data
+//   age: 27,
+//   address: { street: "51 Arena st", city: "Boston" },
+// });
+
+// state.setState({
+//   ...state.data, // or state.get()
+//   email: "rami.alsviri@gmail.com",
+//   password: "123456",
+// }); // update state
+
+// state.delete("password"); // delete password
+// state.delete("user.address.city");
+
+// state.set("user.address.street", "Area 51"); // update value
+// state.set("devices.1", {
+//   id: 2,
+//   type: "phone",
+// }); // add new device
+
+// console.log(state.get()); // or console.log(state.data);
+// // {
+// //   email: 'rami.alsviri@gmail.com',
+// //   devices: [ { id: 1, type: 'PC' }, { id: 2, type: 'phone' } ],
+// //   user: {
+// //     firstName: 'rami',
+// //     lastName: 'sweyri',
+// //     middleName: 'asgm',
+// //     age: 27,
+// //     address: { street: 'Area 51' }
+// //   }
+// // }
+
+// console.log(state.get("user.address")); // {street: "Area 51"}
+
+// state.clear(); // clear state
+// console.log(state.get()); // {}
+
+// state.destroy(); // destroy state
+// console.log(state.get()); // null
 module.exports = State;
